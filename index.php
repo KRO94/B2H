73012d59be91032dcad5f49aa452b3cb7f3f9030
Roman Krovitskiy <roman.krovitskiy@gmail.com>
2017-07-29 02:41:26 +0300

    Створив блок contact-block

--- index.php
+++ index.php
@@ -5,4 +5,5 @@
 	<?php require_once 'pages/gallery_block.php'; ?>
 	<?php require_once 'pages/services_block.php'; ?>
 	<?php require_once 'pages/blog_block.php'; ?>
+	<?php require_once 'pages/contact_block.php'; ?>
 	<?php require_once 'footer.php'; ?>
