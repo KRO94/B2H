6219d9003f615ffb4110495581da0818e3cda361
Roman Krovitskiy <roman.krovitskiy@gmail.com>
2017-07-28 11:22:59 +0300

    Створив блок method-block

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 	<?php require_once 'header.php'; ?>
 	<?php require_once 'pages/block_after_header.php'; ?>
 	<?php require_once 'pages/block_video.php'; ?>
+	<?php require_once 'pages/method_block.php'; ?>
 	<?php require_once 'footer.php'; ?>
