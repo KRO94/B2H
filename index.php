b370cd9f9da4e0438984a938e024d2d6087dc867
Roman Krovitskiy <roman.krovitskiy@gmail.com>
2017-07-28 20:12:54 +0300

    Створив блок services-block

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 	<?php require_once 'pages/block_video.php'; ?>
 	<?php require_once 'pages/method_block.php'; ?>
 	<?php require_once 'pages/gallery_block.php'; ?>
+	<?php require_once 'pages/services_block.php'; ?>
 	<?php require_once 'footer.php'; ?>
---
+++ pages/services_block.php
@@ -0,0 +1,39 @@
+<div class="services-block">
+	<div class="container">
+		<div class="title"><hr>Услуги и цены</div>
+		<div class="service-wrapper clearfix">
+			<div class="serviceBlock">
+				<div class="service">
+					<a href="#" class="name">Пробный сеанс<br><hr></a>
+					<img src="img/service_1.png" alt="Пробный сеанс" class="img-responsive">
+					<div class="price">300 <span>грн.</span></div>
+					<span>(Длительность сеанса 60 мин)</span>
+				</div>
+			</div>
+			<div class="serviceBlock">
+				<div class="service">
+					<a href="#" class="name">Последующие сеансы<br><hr></a>
+					<img src="img/service_2.png" alt="Пробный сеанс" class="img-responsive">
+					<div class="price">400 <span>грн.</span></div>
+					<span>(Длительность сеанса 60 мин)</span>
+				</div>
+			</div>
+			<div class="serviceBlock">
+				<div class="service">
+					<a href="#" class="name">5 сеансов<br><hr></a>
+					<img src="img/service_3.png" alt="Пробный сеанс" class="img-responsive">
+					<div class="price">1800 <span>грн.</span></div>
+					<span>(Длительность сеанса 60 мин)</span>
+				</div>
+			</div>
+			<div class="serviceBlock">
+				<div class="service">
+					<a href="#" class="name">10 сеансов<br><hr></a>
+					<img src="img/service_4.png" alt="Пробный сеанс" class="img-responsive">
+					<div class="price">3500 <span>грн.</span></div>
+					<span>(Длительность сеанса 60 мин)</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
