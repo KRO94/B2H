26c0a265fe6ca85b40f09ba1b903ffee71ce3b81
Roman Krovitskiy <roman.krovitskiy@gmail.com>
2017-07-30 00:05:58 +0300

    Створив сторінку gallery

---
+++ pages/gallery_page.php
@@ -0,0 +1,41 @@
+<div class="gallery-page">
+	<div class="container">
+		<div class="img-wrapper clearfix">
+			<div class="block">
+				<div class="img">
+					<img src="img/blog-post-1.png" alt="" class="img-responsive">
+				</div>
+				<div class="text">Кабинет массажа</div>
+			</div>
+			<div class="block">
+				<div class="img">
+					<img src="img/blog-post-2.png" alt="" class="img-responsive">
+				</div>
+				<div class="text">Клиенты</div>
+			</div>
+			<div class="block">
+				<div class="img">
+					<img src="img/blog-post-3.png" alt="" class="img-responsive">
+				</div>
+				<div class="text">Клиенты</div>
+			</div>
+			<div class="block">
+				<div class="img">
+					<img src="img/blog-post-4.png" alt="" class="img-responsive">
+				</div>
+				<div class="text">Кабинет массажа</div>
+			</div>
+		</div>
+		<div class="pagination-block">
+			<a class="prev" href="#">Назад</a>
+			<span class="current">1</span>
+			<a href="#">2</a>
+			<a href="#">3</a>
+			<a href="#">4</a>
+			<a href="#">5</a>
+			<a href="#">...</a>
+			<a href="#">12</a>
+			<a class="next" href="#">Вперед</a>
+		</div>
+	</div>
+</div>
