4411d09652d7c3531b9cbe394ff0310b2a113524
Roman Krovitskiy <roman.krovitskiy@gmail.com>
2017-07-31 01:10:52 +0300

    Створив сторінку price

---
+++ pages/price_block.php
@@ -0,0 +1,54 @@
+<div class="price-block">
+	<div class="container">
+		<div class="block-service clearfix">
+			<div class="topBlock">
+				<span><hr> Медицинский массаж</span>
+				<div class="right">
+					<div class="price">300 <span>грн</span></div>
+					<a href="#" class="btn-press reserve">Записаться на сеанс</a>
+				</div>
+			</div>
+			<div class="bottomBlock">
+				<div class="blockImg leftBlock">
+					<div class="img">
+						<img src="img/service_1_1.png" alt="" class="img-responsive">
+					</div>
+				</div>
+				<div class="blockText leftBlock">
+					Цель массажа - привести организм к гармоничному состоянию <br>
+					- улучшает капиллярное кровообращение, <br>
+					- повышает гибкость позвоночника и суставов, <br>
+					- нормализует обмен веществ, <br>
+					- помогает бороться с лишним весом, <br>
+					- снимает стресс, депрессию, хроническую усталость, <br>
+					- повышает работоспособность и быстроту мышления.
+				</div>
+			</div>
+		</div>
+		<div class="block-service clearfix">
+			<div class="topBlock">
+				<span><hr> Восстанавливающий массаж</span>
+				<div class="right">
+					<div class="price">600 <span>грн</span></div>
+					<a href="#" class="btn-press reserve">Записаться на сеанс</a>
+				</div>
+			</div>
+			<div class="bottomBlock">
+				<div class="blockImg leftBlock">
+					<div class="img">
+						<img src="img/service_1_2.png" alt="" class="img-responsive">
+					</div>
+				</div>
+				<div class="blockText leftBlock">
+					Цель массажа - привести организм к гармоничному состоянию <br>
+					- улучшает капиллярное кровообращение, <br>
+					- повышает гибкость позвоночника и суставов, <br>
+					- нормализует обмен веществ, <br>
+					- помогает бороться с лишним весом, <br>
+					- снимает стресс, депрессию, хроническую усталость, <br>
+					- повышает работоспособность и быстроту мышления.
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
