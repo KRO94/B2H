5a8c3fa76689bfe2ee2a76abd07a18e9ac498281
Roman Krovitskiy <roman.krovitskiy@gmail.com>
2017-07-31 02:03:23 +0300

    Створив сторінку contact

---
+++ contact.php
@@ -0,0 +1,5 @@
+	<?php require_once 'header.php'; ?>
+	<?php require_once 'pages/block_after_header_2.php'; ?>
+	<?php require_once 'pages/contact_block_2.php'; ?>
+	<?php require_once 'pages/map_block.php'; ?>
+	<?php require_once 'footer.php'; ?>
---
+++ pages/contact_block_2.php
@@ -0,0 +1,28 @@
+<div class="contact-block pageC">
+	<div class="container">
+		<div class="wrapper clearfix">
+			<div class="leftBlock contact">
+				<div class="title_48"><hr>Контакты</div>
+				<div class="block"><span><i class="fa fa-map-marker" aria-hidden="true"></i></span>Киев, пер. Т. Шевченко, 13/21
+				(ст.м. Майдан Независимости)</div>
+				<div class="block tel"><span><i class="fa fa-phone" aria-hidden="true"></i></span><a href="tel:+380999156955">099 915 69 55</a></div>
+				<div class="block"><span><i class="fa fa-envelope" aria-hidden="true"></i></span><a href="mailto:b2h@mail.com">b2h@mail.com</a></div>
+				<ul class="social">
+					<li><a href="#facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li><a href="#twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+				</ul>
+				<div class="clearfix"></div>
+			</div>
+			<div class="rightBlock form">
+				<div class="title_48"><hr>Обратная связь</div>
+				<div class="wpcf7">
+					<input type="text" name='name' placeholder='Имя'>
+					<input type="text" name='phone' placeholder='Телефон'>
+					<input type="text" name='time' placeholder='Удобное время для звонка менеджера'>
+					<input type="submit" class='btn-press' value='Записаться на сеанс'>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
